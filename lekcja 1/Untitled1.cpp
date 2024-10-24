cb977e0d38a055fe36f6d7c2477e9cd658c78f7e
Igor22334 <gavrilon56@gmail.com>
2024-10-24 10:11:56 +0200

    Komentarz

--- lekcja 1/Untitled1.cpp
+++ lekcja 1/Untitled1.cpp
@@ -1,5 +1,27 @@
-#include <iostream
+#include <iostream>
 using namespace std;
 int main(){
+	int a = 10;
+	int b = 15;
+	int suma = a + b;
+	int roznica = a - b;
+	int iloczyn = a * b; 
+	cout << "liczba a:" << a <<endl;
+	cout << "liczba b:" << b <<endl;
+	cout << "suma:" << suma <<endl;
+	cout << "roznica:" << roznica <<endl;
+	cout << "iloczyn:" << iloczyn <<endl;
+	
+	
+	
+	
+
+
+
+	
+	
+	
+	
+	
 	return 0;
 }
---
+++ lekcja 1/Untitled2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "witaj w programowaniu w c++" <<endl;
+	
+	
+	
+	
+	
+    return 0;
+}
